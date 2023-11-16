28904b648911ce8dda9793beda5c706d187fbc87
zivvvv <guoxiaoshen2021@163.com>
2023-11-16 17:51:34 +0800

    chore: 新增docker部署脚本和Makefile；

---
+++ Makefile
@@ -0,0 +1,12 @@
+default: help
+
+# make release [tag=a.b.c]
+docker-release:
+	# 发布docker
+	./build/docker-release.sh $(tag)
+
+
+help:
+	@printf "   make \033[1m命令使用说明\033[0m\n"
+	@printf "   \033[35mmake docker-release tag=a.b.c		\033[0m\t\033[0m\t\033[0m\t\033[0m\t- docker环境构建发布\n"
+	@printf "   \033[35mmake help					\033[0m\t\033[0m\t\033[0m\t\033[0m\t- 使用帮助\n"
---
+++ build/docker-release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# docker镜像构建并上传
+# 命令: ./docker-release.sh [tag]
+
+set -e
+
+TAG=$1
+
+# 构建最新版本
+docker buildx build --platform linux/amd64 -t shenx2021/file-manager:latest .
+# 登录
+docker login
+# 上传docker hub
+docker push shenx2021/file-manager:latest
+
+# tag不为空，则需要指定版本
+if test -n $TAG ;then
+  # 打上指定tag
+  docker tag shenx2021/file-manager:latest shenx2021/file-manager:$TAG
+  # 上传docker hub
+  docker push shenx2021/file-manager:$TAG
+fi
+
